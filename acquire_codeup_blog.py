40b720cdb794c0781e7601565105260a189dc9c0
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-05-10 12:03:24 -0500

    :book: Added comments and docstrings

--- acquire_codeup_blog.py
+++ acquire_codeup_blog.py
@@ -19,7 +19,6 @@
 
         __init__(self, file_name = 'codeup_blog.csv')
         read_from_source(self)
-        get_all_blog_articles(self)
         get_all_article_links(self, url)
         get_blog_articles(self, urls)
         get_article_info(self, soup)
@@ -39,34 +38,84 @@ from _acquire import Acquire
 
 class AcquireCodeupBlog(Acquire):
 
+    '''
+        A class used for acquiring the Codeup blog articles from the Codeup 
+        website.
+    
+        Instance Fields
+        ---------------
+        file_name: str
+    
+        Instance Methods
+        ----------------
+        __init__: Returns None
+        read_from_source: Returns DataFrame
+        get_all_article_links: Returns list[str]
+        get_blog_articles: Returns list[dict]
+        get_article_info: Returns dict
+    '''
+
     ################################################################################
 
-    def __init__(self, file_name = 'codeup_blog.csv'):
+    def __init__(self, file_name: str = 'codeup_blog.csv'):
+        '''
+            Parameters
+            ----------
+            file_name: str, optional
+                The file name to use when cacheing the data. Default value is 
+                "codeup_blog.csv".
+        '''
+
         self.file_name = file_name
 
     ################################################################################
 
-    def read_from_source(self):
-        return pd.DataFrame(self.get_all_blog_articles())
+    def read_from_source(self) -> pd.DataFrame:
+        '''
+            Acquire the Codeup blog articles data from the source (Codeup website).
+            Return a dataframe containing the data.
+        
+            Returns
+            -------
+            DataFrame: A dataframe containing the title, date, and content for 
+                all Codeup blog articles.
+        '''
 
-    ################################################################################
-
-    def get_all_blog_articles(self):
         url = 'https://codeup.com/blog/'
         links = self.get_all_article_links(url)
-        return self.get_blog_articles(set(links))
+
+        # Remove all duplicates by turning the list into a set and return as a dataframe.
+        return pd.DataFrame(self.get_blog_articles(set(links)))
 
     ################################################################################
 
-    def get_all_article_links(self, url):
-        links = []
+    def get_all_article_links(self, url: str) -> list[str]:
+        '''
+            Recursively acquire the links to all Codeup blog posts and return 
+            a list containing all the URLs. This function will recursively 
+            call itself until it reaches the final page of blog articles.
+        
+            Parameters
+            ----------
+            url: str
+                The URL of the blog page containing links to Codeup blog posts.
+        
+            Returns
+            -------
+            list[str]: A list of URLs linking to all Codeup blog posts.
+        '''
+
+        # Set the header so we can have access to scrape the Codeup website and acquire the HTML code
         headers = {'user-agent' : 'Innis Data Science Cohort'}
         response = requests.get(url, headers = headers)
         soup = BeautifulSoup(response.text, 'html.parser')
-        
-        for article in soup.find_all('article'):
-            links.append(article.find('a')['href'])
+
+        # For each article element on the page get the href attribute of the link element.
+        # Create a list of all the URLs.
+        links = [article.find('a')['href'] for article in soup.find_all('article')]
             
+        # If an Older Entries link exists on the page, call this function again with that URL.
+        # Add the returned list to the list of links.
         if (link := soup.find('a', string = '« Older Entries')):
             links += self.get_all_article_links(link['href'])
         
@@ -74,11 +123,31 @@ class AcquireCodeupBlog(Acquire):
 
     ################################################################################
 
-    def get_blog_articles(self, urls):
+    def get_blog_articles(self, urls: list[str]) -> list[dict]:
+        '''
+            Acquire the article info for all the URLs provided and return as a 
+            list of dictionaries.
+        
+            Parameters
+            ----------
+            urls: list[str]
+                A list of URLs to scrape.
+        
+            Returns
+            -------
+            list[dict]: A list of dictionaries containing the article info for 
+                each article linked by the provided URLs.
+        '''
+
         articles = []
+
+        # Set the headers so we have access to scrape the Codeup website.
         headers = {'user-agent' : 'Innis Data Science Cohort'}
         
         for url in urls:
+
+            # Attempt to get a response from the server. If something goes 
+            # wrong skip ahead to the next URL.
             try:
                 response = requests.get(url, headers = headers)
             except requests.exceptions.RequestException:
@@ -86,6 +155,8 @@ class AcquireCodeupBlog(Acquire):
                 
             soup = BeautifulSoup(response.text, 'html.parser')
             
+            # Make sure the URL that we received is the same as the one we sent the request
+            # for, in case the server redirects us.
             if url == response.url:
                 articles.append(self.get_article_info(soup))
             
@@ -93,7 +164,23 @@ class AcquireCodeupBlog(Acquire):
 
     ################################################################################
 
-    def get_article_info(self, soup):
+    def get_article_info(self, soup: BeautifulSoup) -> dict:
+        '''
+            Return a dictionary containing the title, date, and content of the 
+            Codeup blog post used to create the BeautifulSoup object soup.
+        
+            Parameters
+            ----------
+            soup: BeautifulSoup
+                A BeautifulSoup object that was created using the requests 
+                response from a Codeup blog article.
+        
+            Returns
+            -------
+            dict: A dictionary containing the title, date, and content of a 
+                Codeup blog article.
+        '''
+
         return {
             'title' : soup.find('h1', class_ = 'entry-title').text,
             'date' : soup.find('p', class_ = 'post-meta').find('span').text,
--- acquire_news_articles.py
+++ acquire_news_articles.py
@@ -38,14 +38,52 @@ from _acquire import Acquire
 
 class AcquireNewsArticles(Acquire):
 
+    '''
+        A class used for acquiring news article data from inshorts.com.
+    
+        Instance Fields
+        ---------------
+        file_name: str
+    
+        Instance Methods
+        ----------------
+        __init__: Returns None
+        read_from_source: Returns DataFrame
+        get_news_articles: Returns DataFrame
+        get_category_links: Returns list[str]
+        get_article_info: Returns dict
+    '''
+
     ################################################################################
 
     def __init__(self, file_name: str = 'inshorts_articles.csv'):
+        '''
+            Parameters
+            ----------
+            file_name: str
+                The file name to use when cacheing the data.
+        '''
+
         self.file_name = file_name
 
     ################################################################################
 
-    def read_from_source(self):
+    def read_from_source(self) -> pd.DataFrame:
+        '''
+            Acquire the inshorts news articles data from the source.
+        
+            Parameters
+            ----------
+            parameter: parameter_type
+                parameter_description
+        
+            Returns
+            -------
+            DataFrame: A dataframe containing the title, content, and category 
+                for the categories business, sports, technology, and 
+                entertainment.
+        '''
+
         return self.get_news_articles([
             'business',
             'sports',
@@ -55,9 +93,25 @@ class AcquireNewsArticles(Acquire):
 
     ################################################################################
 
-    def get_news_articles(self, categories):
+    def get_news_articles(self, categories: list[str]) -> pd.DataFrame:
+        '''
+            Return a dataframe containing the title, content, and category for 
+            each news article in the provided categories.
+        
+            Parameters
+            ----------
+            categories: list[str]
+                A list of categories to acquire data from on inshorts.com.
+        
+            Returns
+            -------
+            DataFrame: A dataframe containing the title, content, and caegory
+                for each new article.
+        '''
+
         articles = []
 
+        # For each category, and each link in each category, get the article info that we need.
         for category in categories:
             for link in self.get_category_links(category):
                 articles.append(self.get_article_info(link, category))
@@ -66,21 +120,53 @@ class AcquireNewsArticles(Acquire):
 
     ################################################################################
 
-    def get_category_links(self, category):
-        links = []
+    def get_category_links(self, category: str) -> list[str]:
+        '''
+            Get the links for all articles in the provided category page.
+        
+            Parameters
+            ----------
+            category: str
+                The category page to scrape.
+        
+            Returns
+            -------
+            list[str]: A list of URLs containing links to all news articles on 
+                the category page.
+        '''
+
+        # Build the URL we need to scrape and ping the server.
         base_domain = 'https://inshorts.com'
         url = f'{base_domain}/en/read/{category}'
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        
-        for card in soup.find_all('div', class_ = 'news-card-title'):
-            links.append(base_domain + card.find('a')['href'])
+
+        # For each news card acquire the link to the article and build the URL by adding the base domain to the link.
+        links = [base_domain + card.find('a')['href'] for card in soup.find_all('div', class_ = 'news-card-title')]
             
         return links
 
     ################################################################################
 
-    def get_article_info(self, url, category):
+    def get_article_info(self, url: str, category: str) -> dict:
+        '''
+            Return a dictionary containing the title, content, and category for 
+            the provided news article URL.
+        
+            Parameters
+            ----------
+            url: str
+                A URL linking to a news article.
+
+            category: str
+                The category of the news article.
+        
+            Returns
+            -------
+            dict: A dictionary containing the title, content, and category for 
+                the news article.
+        '''
+
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
